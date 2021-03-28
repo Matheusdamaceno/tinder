5b068661ad1e583084f1f5c9a0b44597c2dca2e7
Matheusdamaceno <damaceno.martins@hotmail.com>
2021-03-28 05:51:32 +0000

    tinder

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sobre mim:</h1>
+        <img src="/public/images/lucas.jpg"/>
+        <h2>Lucas Marsola, 16 anos.</h2>
+    <p>Meu nome é Lucas Marsola, moro no interior de São Paulo, mas especificamente em Palmital</p>
+    <p>Sou estudante no Centro Paula Souza, Etec Mario Antonio Verza daqui da cidade mesmo. 
+        Neste ano estou cursando o terceiro ano de administração e terceiro ano do ensino médio.</p>
+    <p>Sou muito engraçado, sou bastante extrovertido, gosto de sair, baforar um lolo e curtir com meus amigos</p>
+      </header>
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
